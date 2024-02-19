42d16b9e408cc3994bfa738f8da9561ae8cd057c
thapanankulab25 <nicekrubma123@gmail.com>
2024-02-19 09:36:30 +0700

    create search form

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import FormSearch  from './form-search';
+import FormBootstrap from './form-bootstrap'
+
 export default function App() {
 
   return <>
   <FormSearch/>
+  <br>
+  </br>
+  <FormBootstrap/>
   </>
 }
